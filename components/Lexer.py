886e6a18b3ae12b3dc51afbb38232c7a43c007b3
Ashley Davies-Lyons <ashley@daviesl.uk>
2015-03-20 19:57:38 +0000

    Commit Test

--- components/Lexer.py
+++ components/Lexer.py
@@ -3,3 +3,5 @@ __author__ = 'Ashley'
 
 class Lexer():
     """ The Lexer is a mystical tool from the realms of Gondor that turns a file into meaningful tokens """
+    def __init__(self):
+        pass
